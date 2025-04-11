a260ec27cfa30039560e8eba92b508557099f95b
surekha <surekha.baral.73@gmail.com>
2025-04-11 23:00:36 +1000

    newer changes

--- app/src/main/java/com/example/taskmanagement/Userdetails.java
+++ app/src/main/java/com/example/taskmanagement/Userdetails.java
@@ -1,6 +1,9 @@
 package com.example.taskmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,11 @@ public class Userdetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_userdetails);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainlayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
